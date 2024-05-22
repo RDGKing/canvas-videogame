f121b80ce4a9290b1cd4e40605f1999c2352049a
Angel Rodrigo <arbyking102@gmail.com>
2024-05-21 21:51:55 -0600

    Removed unnecessary functions and added picture and background

--- js/main.js
+++ js/main.js
@@ -7,36 +7,35 @@ let canvasHeight = 400;
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
+var imagen = new Image();
+imagen.src = 'images/fondo.png'; // Reemplaza esto con la ruta a tu imagen de fondo
 
-// Función para generar colores RGB aleatorios
-function generarColorRGB() {
-    var r = Math.floor(Math.random() * 256); // Valor de rojo entre 0 y 255
-    var g = Math.floor(Math.random() * 256); // Valor de verde entre 0 y 255
-    var b = Math.floor(Math.random() * 256); // Valor de azul entre 0 y 255
-    var color_rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
-    return color_rgb;
-}
 
-// Clase Circle
+/////////CLASE CIRCULO/////////
 class Circle {
-    constructor(x, y, radius, color, text, speed) {
+    constructor(x, y, radius, speed, imageUrl) {
         this.posX = x;
         this.posY = y;
         this.radius = radius;
-        this.color = color;
-        this.text = text;
         this.speed = speed;
         this.dx = 1 * this.speed;
         this.dy = -1 * this.speed; // Cambiar a dirección hacia arriba
+        this.imageUrl = imageUrl; // Esta línea es la correcta
+
+        // Cargar la imagen
+        this.image = new Image();
+        this.image.src = imageUrl;
+
+
+
     }
 
     draw(context) {
+
+
+        context.drawImage(this.image, this.posX - this.radius, this.posY - this.radius, this.radius * 2, this.radius * 2);
         context.beginPath();
-        context.strokeStyle = this.color;
-        context.textAlign = "center";
-        context.textBaseline = "middle";
-        context.font = "20px Arial";
-        context.fillText(this.text, this.posX, this.posY);
+        context.strokeStyle = "black";
         context.lineWidth = 3;
         context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2, false);
         context.stroke();
@@ -70,31 +69,39 @@ class Circle {
         this.posY += this.dy;
     }
 }
+/////////CLASE CIRCULO/////////
 
-// Función para obtener la distancia entre dos puntos
+
+
+/////// Función para obtener la distancia entre dos puntos//////////
 function getDistance(posX1, posY1, posX2, posY2) {
     let result = Math.sqrt(Math.pow((posX2 - posX1), 2) + Math.pow((posY2 - posY1), 2));
     return result;
 }
+/////// Función para obtener la distancia entre dos puntos//////////
 
+
+
+/////////////CREA CIRCULOS//////////
 let arrayCircle = [];
 
 for (let i = 0; i < 10; i++) {
-    let randomR = Math.floor(Math.random() * 30 + 15);
+    let randomR = Math.floor(Math.random() * 40 + 30);
     let randomX, randomY;
-
     // Generar coordenadas aleatorias hasta que no se superpongan con otros círculos
     do {
         randomX = Math.random() * (canvasWidth - randomR * 2) + randomR;
         randomY = canvasHeight + Math.random() * (canvasHeight - randomR * 2) + randomR;
     } while (checkOverlap(randomX, randomY, randomR)); // Verificar superposición
-
-    let randomS = Math.floor(Math.random() * 3 + 2);
-
-    let miCirculo = new Circle(randomX, randomY, randomR, generarColorRGB(), (i + 1), randomS);
+    let randomS = Math.floor(Math.random() * 2 + 1);
+    let imageUrl = 'images/esfera.png'; // Cambiar a la ruta de tu imagen
+    let miCirculo = new Circle(randomX, randomY, randomR, 1, imageUrl);
     arrayCircle.push(miCirculo);
 }
+/////////////CREA CIRCULOS//////////
 
+
+///////////Controla superposicion////////////
 function checkOverlap(x, y, radius) {
     // Verificar si el círculo generado está superpuesto con algún otro círculo existente
     for (let i = 0; i < arrayCircle.length; i++) {
@@ -106,9 +113,15 @@ function checkOverlap(x, y, radius) {
     }
     return false; // No hay superposición
 }
+///////////Controla superposicion////////////
 
+
+//////////REBOTE DE CIRCULOS/////////////
 function updateCircles() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.drawImage(imagen, 0, 0, canvasWidth, canvasHeight); // Dibujar la imagen de fondo nuevamente
+
+    
     arrayCircle.forEach(circle => {
         circle.update(ctx);
 
@@ -119,8 +132,6 @@ function updateCircles() {
                 let angulo = Math.atan2(circletwo.posY - circle.posY, circletwo.posX - circle.posX);
                 //console.log("angulo: " + angulo * 180 / Math.PI);
 
-                circle.color = generarColorRGB();
-                circletwo = generarColorRGB();
                 let PosNewX = Math.cos(angulo);
                 let PosNewY = Math.sin(angulo);
                 // console.log("X: " + PosNewX * 180 / Math.PI);
@@ -135,11 +146,15 @@ function updateCircles() {
     });
     requestAnimationFrame(updateCircles);
 }
-
 updateCircles();
+//////////REBOTE DE CIRCULOS/////////////
 
-// Event listener para el clic del mouse en el canvas
+
+
+
+////////7///////// Event listener para el clic del mouse en el canvas///////////////////
 canvas.addEventListener("click", function (event) {
+    
     let rect = canvas.getBoundingClientRect(); // Obtener las dimensiones y posición del canvas
     let mouseX = event.clientX - rect.left; // Coordenada X del clic ajustada al canvas
     let mouseY = event.clientY - rect.top; // Coordenada Y del clic ajustada al canvas
@@ -163,3 +178,6 @@ canvas.addEventListener("click", function (event) {
         circle.draw(ctx);
     });
 });
+
+
+////////7///////// Event listener para el clic del mouse en el canvas///////////////////
